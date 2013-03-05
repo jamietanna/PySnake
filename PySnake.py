baf611bb787cd4ef90fa1ef8de96d6c32c605091
Jamie Tanna <jvt02u@cs.nott.ac.uk>
2013-03-05 13:53:09 +0000

    Implemented food expiry time, game over screen
    git-svn-id: https://code.cs.nott.ac.uk/svn/jvt02u-g51fse@8 a8e551cc-84c7-11e2-9eb5-0bd010c48d73

--- PySnake.py
+++ PySnake.py
@@ -4,33 +4,36 @@
 import pygame
 import random
 import math
+import sys
 
 ################################################ 
 #### Globals
 ################################################ 
 
-FOOD_SPEED_INCREASE_STEP = 5
+FOOD_SPEED_INCREASE_STEP = 10
 FOOD_SPEED_INCREASE_AMT = 1
 FOOD_LENGTH_INCREASE = 20
+FOOD_EXPIRE_TIME = 5 # seconds
 
 #### TODO: allow user to specify "mode" - denotes initial speed/steps etc
-
+#### TODO: allow floating point speed :. can increment a lot easier
 INITIAL_LENGTH = 50
-INITIAL_SPEED = 2
+INITIAL_SPEED = 1
 
 FPS = 120
 
 #### TODO; use argv to allow user to specify different resolution
 
-WIDTH = 640
-HEIGHT = 400
-TOP_GUTTER = 20
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 400
 
 ## ballS
 
-SIZE = 10
-number_of_balls = 2
-my_balls = []
+#### TODO: Initial num balls determined by the level of difficulty
+
+BALL_SIZE = 10
+NUMBER_OF_BALLS = 2
+MY_BALLS = []
 BALL_COUNT = 0 
 
 ################################################ 
@@ -100,6 +103,7 @@ class Snake:
 #            pygame.draw.rect(self.surface,self.colour, (i[0],i[1], 5,),2)
 
 
+#### TODO: ensure that food spawns in reachable place
 #### TODO: if not collected within certain time, disappears
 class Food:
     def __init__(self, surface):
@@ -107,6 +111,7 @@ class Food:
         self.x = random.randint(0, (surface.get_width() - 10))
         self.y = random.randint(0, (surface.get_height() - 10))
         self.color = 255, 255, 255
+        self.time = FOOD_EXPIRE_TIME * FPS # otherwise clock ticks far too much and we lose it
 
     def draw(self):
         pygame.draw.rect(self.surface, self.color, (self.x, self.y, 10, 10), 0)
@@ -121,14 +126,20 @@ class Food:
             return False
         else:
             return True
+    def expire(self):
+	self.time -= 1 #decrement
+	if self.time <= 0:
+	    return 1
+	else:
+	    return 0
 
 ## BALL CLASS
 
 class Ball():
-    def __init__(self, (x, y), SIZE):
+    def __init__(self, (x, y), BALL_SIZE):
         self.x = x
         self.y = y
-        self.size = SIZE
+        self.size = BALL_SIZE
         self.colour = (0, 0, 255)
         self.thickness = 1
         self.speed = 2
@@ -142,16 +153,16 @@ class Ball():
         self.y -= math.cos(self.angle) * self.speed
 
     def bounce(self):
-        if self.x > WIDTH - self.size:
-            self.x = 2*(WIDTH - self.size) - self.x
+        if self.x > SCREEN_WIDTH - self.size:
+            self.x = 2*(SCREEN_WIDTH - self.size) - self.x
             self.angle = - self.angle
 
         elif self.x < self.size:
             self.x = 2*self.size - self.x
             self.angle = - self.angle
 
-        if self.y > HEIGHT - self.size:
-            self.y = 2*(HEIGHT - self.size) - self.y
+        if self.y > SCREEN_HEIGHT - self.size:
+            self.y = 2*(SCREEN_HEIGHT - self.size) - self.y
             self.angle = math.pi - self.angle
 
         elif self.y < self.size:
@@ -168,18 +179,18 @@ class Ball():
 
 # drawing the balls
 
-for n in range(number_of_balls):
+for n in range(NUMBER_OF_BALLS):
     
-    x = random.randint(SIZE, WIDTH)
-    y = random.randint(SIZE, HEIGHT)
-    ball = Ball((x, y), SIZE)
+    x = random.randint(BALL_SIZE, SCREEN_WIDTH)
+    y = random.randint(BALL_SIZE, SCREEN_HEIGHT)
+    ball = Ball((x, y), BALL_SIZE)
     ball.speed = 1
     ball.angle = random.uniform(0, math.pi*2)
-    my_balls.append(ball)
+    MY_BALLS.append(ball)
 
 # initialise our screen, clock
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
 
@@ -194,27 +205,33 @@ running = True
 # create the score sprite
 pygame.font.init()
 
-
+#scoreFont = pygame.font.SysFont("monospace", 18)
+smallFont= pygame.font.SysFont("monospace", 18)
+bigFont = pygame.font.SysFont("monospace", 24)
+    
 while running:
+    clock.tick(FPS)
     screen.fill((0, 0, 0))
     snake.move()
     snake.draw()
     food.draw()
-    for ball in my_balls:
+    for ball in MY_BALLS:
         ball.move()
         ball.bounce()
         ball.display()
 
     # http://stackoverflow.com/questions/10077644/python-display-text-w-font-color
-    myfont = pygame.font.SysFont("monospace", 18)
-    label = myfont.render("Score: "+str(snake.num_eaten), 1, (255,255,0))
-    screen.blit(label, (0, 0))
+    score = smallFont.render("Score: "+str(snake.num_eaten), 1, (255,255,0))
+    screen.blit(score, (0, 0))
+    
+    timeLeft = smallFont.render("Time left: "+str(food.time), 1, (255,255,0))
+    screen.blit(timeLeft, (0, 10))
     
     if snake.crashed:
         running = False
-    elif snake.x <= 0 or snake.x >= WIDTH-1:
+    elif snake.x <= 0 or snake.x >= SCREEN_WIDTH-1:
         running = False
-    elif snake.y <= 0 or snake.y >= HEIGHT-1:
+    elif snake.y <= 0 or snake.y >= SCREEN_HEIGHT-1:
         running = False
     elif food.check(snake.x,snake.y):
         snake.eat()
@@ -223,16 +240,19 @@ while running:
         # spawns more balls when eaten
         BALL_COUNT+=1
         if (BALL_COUNT == 1):
-            x = random.randint(SIZE, WIDTH)
-            y = random.randint(SIZE, HEIGHT)
-            ball = Ball((x, y), SIZE)
+            x = random.randint(BALL_SIZE, SCREEN_WIDTH)
+            y = random.randint(BALL_SIZE, SCREEN_HEIGHT)
+            ball = Ball((x, y), BALL_SIZE)
             ball.speed = 1
             ball.angle = random.uniform(0, math.pi*2)
-            my_balls.append(ball)
+            MY_BALLS.append(ball)
             BALL_COUNT = 0
+    elif food.expire():
+	# if we've run out of time to collect the food, then respawn it
+	food = Food(screen)
 
     # only if head collides with ball,tail is OK
-    for ball in my_balls:
+    for ball in MY_BALLS:
         if ball.collide(snake.x,snake.y):
             running = False
 
@@ -243,8 +263,37 @@ while running:
             snake.key_event(event)
 
     pygame.display.flip()
-    clock.tick(FPS)
+    #clock.tick(FPS)
 
-# here we have finished, therefore show scores etc (in the console)
-# however, this is going to be quite useless now we're showing it on screen
-print "You successfully got a total of", snake.num_eaten, "pieces of food!"
+### For typography
+if running == False:
+    # here we have finished, therefore show scores etc (in the console)
+    # however, this is going to be quite useless now we're showing it on screen
+    #### TODO: Scoring system based on difficulty
+    #### TODO: Score is increased dependant on time - aka you get more points for getting the food when there's more time on the clock
+    #### TODO: Output score on game over, cleared screen
+    #### TODO: Top scores
+    print "You successfully got a total of", snake.num_eaten, "pieces of food!"
+    
+    #### TODO: inside the blit() ???
+    
+    gameOver = bigFont.render("Game Over! You lost.", 1, (255,255,0))
+    screen.blit(gameOver, (SCREEN_WIDTH/2, (SCREEN_HEIGHT/2) - 40))
+    
+    finalScore = bigFont.render("Your final score was: "+str(snake.num_eaten), 1, (255,255,0))
+    screen.blit(finalScore, (SCREEN_WIDTH/2, (SCREEN_HEIGHT/2) - 20))
+    
+    anyKey = bigFont.render("Press any key to exit.", 1, (255,255,0))
+    screen.blit(anyKey, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+    
+    pygame.display.flip()
+    
+    
+    # Not a nice hack - keep going until we hit a KEYDOWN
+    while True:
+      for event in pygame.event.get():
+	  if event.type == pygame.KEYDOWN:
+	      sys.exit()
+	  else:
+	      pass
+    
